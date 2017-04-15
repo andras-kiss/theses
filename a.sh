36ce98d3ccdd7fadee338fb4ba5407763c8ef3c3
andras-kiss <akiss@gamma.ttk.pte.hu>
2017-04-16 01:22:16 +0200

    hun version started

--- a.sh
+++ a.sh
@@ -1,4 +1,6 @@
 pdflatex -shell-escape theses.tex
 pdflatex -shell-escape theses.tex
+pdflatex -shell-escape theses_hun.tex
+pdflatex -shell-escape theses_hun.tex
 
 rm *.{aux,bbl,blg,lof,log,lot,out,toc}
