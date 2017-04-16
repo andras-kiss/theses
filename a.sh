545551f8e40b5d2e9f22a38f553e6153ede9a6d6
andras-kiss <akiss@gamma.ttk.pte.hu>
2017-04-16 15:02:49 +0200

    magyar done

--- a.sh
+++ a.sh
@@ -1,6 +1,6 @@
-pdflatex -shell-escape theses.tex
-pdflatex -shell-escape theses.tex
-pdflatex -shell-escape theses_hun.tex
-pdflatex -shell-escape theses_hun.tex
+pdflatex -shell-escape thesis.tex
+pdflatex -shell-escape thesis.tex
+pdflatex -shell-escape thesis_hun.tex
+pdflatex -shell-escape thesis_hun.tex
 
 rm *.{aux,bbl,blg,lof,log,lot,out,toc}
